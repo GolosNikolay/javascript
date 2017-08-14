9266c4a2bbde607f0de5cfab542cd67d01efff6b
NGOLOS\NGolos <nikolay.golos@firstlinesoftware.com>
2017-08-14 16:54:54 +0300

    information about - "use strict", variables and constaants

--- myScript.js
+++ myScript.js
@@ -1,3 +1,16 @@
-alert('Я - JavaScript!');
-alert('Я - JavaScript!');
-alert('Я - JavaScript!');
+"use strict"; // чтобы работать по стандарту ecmaScript это ставится в начало документа
+			  // однако не поддерживается в IE9, чтобы это исправить можно для старых браузеров подключить
+			  // библиотеку ES5 shim
+
+var CORPORATE_ORANGE = "#f58220"; //константы
+var CORPORATE_BLUE = "#0054a6";
+
+var message = "My first variable";
+console.log(message);
+
+
+
+error = "U can't define variable without var using 'use strict'!";
+console.log(error);
+
+
