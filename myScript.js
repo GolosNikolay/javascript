523608c80304dfe3b0726c5170de7685a5056251
NGOLOS\NGolos <nikolay.golos@firstlinesoftware.com>
2017-08-14 18:22:30 +0300

    Alert, prompt and confirm test!

--- myScript.js
+++ myScript.js
@@ -1,13 +1,27 @@
 "use strict"; // чтобы работать по стандарту ecmaScript это ставится в начало документа
 			  // однако не поддерживается в IE9, чтобы это исправить можно для старых браузеров подключить
 			  // библиотеку ES5 shim
+var error = "Ублюдок, мать твою, ты идиот, поэтому пиши 'Да'!";
+var secondError = "Ты полный идиот раз не нажал OK";
+var check = true;
+var name = "Привет, Николай!";
+alert(name);
 
-//Постфиксный инкремент
-var i = 1;
-var a = i++;
-console.log(a); //a = 1
-
-//Префиксный инкремент
-var i = 1;
-var a = ++i;
-console.log(a); //a = 2
+while (check){
+	var warning = "Ты идиот?";
+	var result = prompt(warning, 'Да');
+	if (result != 'Да'){
+		alert(error);
+	}
+	else{
+		check = false;
+		var lastResult = confirm("Чтобы окончательно убедиться, что ты идиот нажмите 'OK'");
+		if (!lastResult) {
+			alert(secondError);
+			check = true;
+		}
+		else {
+			alert("Поздравляю, ты окончательно идиот!");
+		}
+	}
+}
