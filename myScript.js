c3af6b45763476e264a4a523ee5941ec0cbe6429
NGOLOS\NGolos <nikolay.golos@firstlinesoftware.com>
2017-08-14 19:00:03 +0300

    learned if, else construction and a > b ? true : false;

--- myScript.js
+++ myScript.js
@@ -1,27 +1,16 @@
 "use strict"; // чтобы работать по стандарту ecmaScript это ставится в начало документа
 			  // однако не поддерживается в IE9, чтобы это исправить можно для старых браузеров подключить
 			  // библиотеку ES5 shim
-var error = "Ублюдок, мать твою, ты идиот, поэтому пиши 'Да'!";
-var secondError = "Ты полный идиот раз не нажал OK";
-var check = true;
-var name = "Привет, Николай!";
-alert(name);
 
-while (check){
-	var warning = "Ты идиот?";
-	var result = prompt(warning, 'Да');
-	if (result != 'Да'){
-		alert(error);
-	}
-	else{
+var check = true, result, age;
+while(check){
+	age = prompt("Сколько вам лет?",'');
+	result = age >= 18 ? true : false;
+	if (result){
+		alert("Поздравляю, ты можешь смотреть страницу!");
 		check = false;
-		var lastResult = confirm("Чтобы окончательно убедиться, что ты идиот нажмите 'OK'");
-		if (!lastResult) {
-			alert(secondError);
-			check = true;
-		}
-		else {
-			alert("Поздравляю, ты окончательно идиот!");
-		}
+	}
+	else {
+		alert("Ты еще малой!");
 	}
 }
