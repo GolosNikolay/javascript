4ba324975d22f27d9370e9505373ced0ed56fd93
NGOLOS\NGolos <nikolay.golos@firstlinesoftware.com>
2017-08-15 13:33:06 +0300

    Фукнции с примерами ? и ||

--- myScript.js
+++ myScript.js
@@ -4,26 +4,28 @@
 
 //Преобразования
 
-var text = null, result;
-result = String(text) === "null" ? true : false;
-if (result) {
-	alert("Variable text has text type!");
+function checkName(name){
+	if (name == 'null' || name == ''){
+		return ('Вы не ввели имя!');
+	}
+	return ('Ваше имя ' + name + '!');
 }
 
-var number = "123";
-alert(typeof(+number));
-
-/*" -9\n" + 5 = " -9\n5"
-" -9\n" - 5 = -14
-true + false = 1;
+var result = checkName(prompt("Ведите ваше имя", ''));
+alert(result);
 
 
-null + 1 = 1 // (1)
-undefined + 1 = NaN // (2)
-null == "\n0\n" = false // (3)
-+null == +"\n0\n" = true // (5)
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Родители разрешили?');
+  }
+}
 
-1. null при численном преобразовании становится 0
-2. undefined при численном преобразовании становится NaN
-3. При сравнении == с null преобразования не происходит, есть жёсткое правило: null == undefined и только.
-4. И левая и правая часть == преобразуются к числу 0.*/
+function checkAge(age){
+	return age > 18 ? true : confirm('Родители разрешили?');
+}
+function checkAge(age){
+	return (age > 18) || confirm('Родители разрешили?');
+}
