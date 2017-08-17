21fd5a1938eeaf21f0c3559945660358e4958a03
NGOLOS\NGolos <nikolay.golos@firstlinesoftware.com>
2017-08-17 17:27:17 +0300

    Реализация счетчика через функцию с методами (Замыкания)

--- myScript.js
+++ myScript.js
@@ -2,18 +2,73 @@
 			  // однако не поддерживается в IE9, чтобы это исправить можно для старых браузеров подключить
 			  // библиотеку ES5 shim
 
-//Именнованые аргументы - передача в качества параметра для функции объекта
-//Можно использовать, например, когда нужно изменить некоторые параметры использующиеся по умолчанию
 
-function createModal(parametrs) {
-	var width = parametrs.width || 150;
-	var height = parametrs.height || 'auto';
-	var backgound = parametrs.backgound || '#fafafa';
-	var title = parametrs.title || 'Внимание!';
-	var titleColor = parametrs.titleColor || 'Orange';
+// Замыкаие через вложенную функцию
+function counter() {
+	var counter = 1;
+	return function() {
+		return counter++;
+	};
 }
 
-createModal({
-	width: 400,
-	title: 'Будьте добры!'
-});
+var newCounter = counter();
+var nextCounter = counter();
+
+//Замыкание через вложенную функцию с дополнительными методами
+function objCounter() {
+	var currentCounter = 1;
+
+	function counter() {
+		return currentCounter++;
+	}
+	counter.setValue = function(value) {
+		currentCounter = value;
+	}
+	counter.reset = function() {
+		currentCounter = 0;
+	}
+
+	return counter;
+}
+
+var newObjCounter = objCounter();
+newObjCounter.setValue(5);
+
+
+
+
+function makeBuffer() {
+	var bufferString = '';
+
+	function buffer() {
+		return arguments.length > 0 ? buffer.add(arguments[0]) : buffer.show();
+	}
+	buffer.add = function(addString) {
+		bufferString += addString;
+	}
+	buffer.show = function() {
+		return bufferString;
+	}
+	buffer.clear = function() {
+		bufferString = ;
+	}
+	return buffer;
+}
+
+var buffer = makeBuffer();
+
+// добавить значения к буферу
+buffer('Замыкания');
+buffer.clear();
+buffer(' Использовать');
+buffer(' Нужно!');
+
+// получить текущее значение
+alert( buffer() ); // Замыкания Использовать Нужно!
+
+var buffer2 = makeBuffer();
+buffer2(0);
+buffer2(1);
+buffer2(0);
+
+alert( buffer2() ); // '010'
