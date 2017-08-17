303cbbffab4008a40cbde6ac2b237071d85b8fc9
NGOLOS\NGolos <nikolay.golos@firstlinesoftware.com>
2017-08-17 13:46:03 +0300

    Копирование содержимого объекта, ошибка в выводе данных объекта в консоль

--- myScript.js
+++ myScript.js
@@ -30,3 +30,40 @@ console.log( isEmpty(schedule) ); // true
 schedule["8:30"] = "подъём";
 
 console.log( isEmpty(schedule) ); // false
+
+
+
+//Копирование одного объекта в другой
+// ВАЖНО! В переменной лежит ссылка на объект, а не сам объект 
+var person = {
+	name: 'Vasya',
+	profession: 'Sporstman',
+	pat: 'Dog'
+};
+
+var person2 = {};
+for(var key in person) {
+	person2[key] = person[key];
+}
+console.log(person2);
+
+//Пример ошибки консоли, сначала она посчитает microsecond,
+//а потом выведет microsecond = 123459
+var time = {
+  year: 2345,
+  month: 11,
+  day: 10,
+  hour: 11,
+  minute: 12,
+  second: 13,
+  microsecond: 123456
+}
+
+console.log(time); // (*)
+time.microsecond++; // (**)
+
+console.log(time);
+time.microsecond++;
+
+console.log(time);
+time.microsecond++;
